100666c01c2f2d1d65804e9fc0a2539d9ffdd6f5
James <jpebrookes@gmail.com>
2021-01-29 02:35:50 +0000

    convert Navigation from class component

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import { React, useState, Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Container, Button } from "react-bootstrap";
+import { Navbar, Container, Button, Toast } from "react-bootstrap";
 import {
 	MDBNavbar,
 	MDBNavbarNav,
@@ -19,110 +19,84 @@ import {
 } from "mdbreact";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-class Navigation extends Component {
-	state = {
-		collapseIDMenu: "",
-		collapseIDSearch: "",
-		showToast: "",
-	};
 
-	toggleCollapseMenu = (collapseIDMenu) => () => {
-		this.setState((prevState) => ({
-			collapseIDMenu:
-				prevState.collapseIDMenu !== collapseIDMenu ? collapseIDMenu : "",
-		}));
-	};
+function Navigation() {
+	const [show, setShow] = useState(false);
+	const [collapseIDMenu, setShowMenu] = useState(false);
+	const [collapseIDSearch, setShowSearch] = useState(false);
+	const toggleSearch = () => setShowSearch(!collapseIDSearch);
+	const toggleMenu = () => setShowMenu(!collapseIDMenu);
 
-	toggleCollapseSearch = (collapseIDSearch) => () => {
-		this.setState((prevState) => ({
-			collapseIDSearch:
-				prevState.collapseIDSearch !== collapseIDSearch ? collapseIDSearch : "",
-		}));
-	};
+	return (
+		<nav class="hero">
+			<MDBNavbar className="justify-content-end">
+				<Container>
+					<Navbar.Brand as={Link} to="/">
+						<img
+							alt="UEA Logo"
+							src="./uea_logo.png"
+							width="30"
+							height="30"
+							className="d-inline-block align-top"
+						/>{" "}
+						MY UEA
+					</Navbar.Brand>
+					<MDBBtn outline className="ml-auto" onClick={() => setShow(true)}>
+						Post an Article
+					</MDBBtn>
+					<MDBBtn outline className="ml-auto" onClick={() => setShow(true)}>
+						Post an Event
+					</MDBBtn>
+					<Button
+						className="navbar-toggler ml-auto"
+						type="button"
+						// onClick={this.toggleCollapseSearch("navbarCollapse2")}
+						onClick={toggleSearch}
+						alt="Search"
+						title="Search"
+						role="search"
+					>
+						<FontAwesomeIcon icon={faSearch} role="search" alt="Search">
+							Search
+						</FontAwesomeIcon>
+					</Button>
+					<Button
+						className="navbar-toggler"
+						type="submit"
+						// onClick={this.toggleCollapseMenu("navbarCollapse1")}
+						onClick={toggleMenu}
+						alt="Menu"
+						title="Menu"
+						role="navigation"
+					>
+						<FontAwesomeIcon icon={faBars} />
+					</Button>
 
-	toggleShowToast = (showToast) => () => {
-		this.setState((prevState) => ({
-			showToast: prevState.showToast !== showToast ? showToast : "",
-		}));
-	};
+					<MDBCollapse id="navbarCollapse3" isOpen={collapseIDSearch} navbar>
+						<MDBNavbarNav left>
+							<MDBFormInline className="md-form mr-auto m-0">
+								<input
+									className="form-control mr-sm-2"
+									type="text"
+									placeholder="Search"
+									aria-label="Search"
+								/>
+								<MDBBtn
+									outline
+									color="white"
+									size="sm"
+									type="submit"
+									// onClick={this.toggleCollapseSearch("navbarCollapse2")}
+									className="mr-auto btn-search"
+								>
+									Search
+								</MDBBtn>
+							</MDBFormInline>
+						</MDBNavbarNav>
+					</MDBCollapse>
 
-	render() {
-		return (
-			<nav class="hero">
-				<MDBNavbar className="justify-content-end">
-					<Container>
-						<Navbar.Brand as={Link} to="/">
-							<img
-								alt="UEA Logo"
-								src="./uea_logo.png"
-								width="30"
-								height="30"
-								className="d-inline-block align-top"
-							/>{" "}
-							MY UEA
-						</Navbar.Brand>
-						<MDBBtn outline className="ml-auto">
-							Post an Article
-						</MDBBtn>
-						<MDBBtn outline className="ml-auto">
-							Post an Event
-						</MDBBtn>
-						<Button
-							className="navbar-toggler ml-auto"
-							type="button"
-							onClick={this.toggleCollapseSearch("navbarCollapse2")}
-							alt="Search"
-							title="Search"
-							role="search"
-						>
-							<FontAwesomeIcon icon={faSearch} role="search" alt="Search">
-								Search
-							</FontAwesomeIcon>
-						</Button>
-						<Button
-							className="navbar-toggler"
-							type="submit"
-							onClick={this.toggleCollapseMenu("navbarCollapse1")}
-							alt="Menu"
-							title="Menu"
-							role="navigation"
-						>
-							<FontAwesomeIcon icon={faBars} />
-						</Button>
-
-						<MDBCollapse
-							id="navbarCollapse2"
-							isOpen={this.state.collapseIDSearch}
-							navbar
-						>
-							<MDBNavbarNav left>
-								<MDBFormInline className="md-form mr-auto m-0">
-									<input
-										className="form-control mr-sm-2"
-										type="text"
-										placeholder="Search"
-										aria-label="Search"
-									/>
-									<MDBBtn
-										outline
-										color="white"
-										size="sm"
-										type="submit"
-										onClick={this.toggleCollapseSearch("navbarCollapse2")}
-										className="mr-auto btn-search"
-									>
-										Search
-									</MDBBtn>
-								</MDBFormInline>
-							</MDBNavbarNav>
-						</MDBCollapse>
-
-						<MDBCollapse
-							id="navbarCollapse1"
-							isOpen={this.state.collapseIDMenu}
-							navbar
-						>
-							{/* 							<MDBNavItem>
+					<MDBCollapse id="navbarCollapse4" isOpen={collapseIDMenu} navbar>
+						{/* 							<MDBNavItem>
 								<Form inline>
 									<FormControl
 										type="text"
@@ -138,32 +112,43 @@ class Navigation extends Component {
 									</Button>
 								</Form>
 							</MDBNavItem> */}
-							<MDBNavbarNav>
-								<MDBNavItem active>
-									<MDBNavLink
-										as={Link}
-										to="/"
-										onClick={this.toggleCollapseMenu("navbarCollapse1")}
-									>
-										Home
-									</MDBNavLink>
-								</MDBNavItem>
-								<MDBNavItem>
-									<MDBNavLink
-										as={Link}
-										to="/dashboard"
-										onClick={this.toggleCollapseMenu("navbarCollapse1")}
-									>
-										Dashboard
-									</MDBNavLink>
-								</MDBNavItem>
-							</MDBNavbarNav>
-						</MDBCollapse>
-					</Container>
-				</MDBNavbar>
-			</nav>
-		);
-	}
+						<MDBNavbarNav>
+							<MDBNavItem active>
+								<MDBNavLink
+									as={Link}
+									to="/"
+									// onClick={this.toggleCollapseMenu("navbarCollapse1")}
+								>
+									Home
+								</MDBNavLink>
+							</MDBNavItem>
+							<MDBNavItem>
+								<MDBNavLink
+									as={Link}
+									to="/dashboard"
+									// onClick={this.toggleCollapseMenu("navbarCollapse1")}
+								>
+									Dashboard
+								</MDBNavLink>
+							</MDBNavItem>
+						</MDBNavbarNav>
+					</MDBCollapse>
+				</Container>
+			</MDBNavbar>
+
+			<Toast onClose={() => setShow(false)} show={show} delay={2000} autohide>
+				<Toast.Header>
+					<strong className="mr-auto">Error</strong>
+
+					<small>Just now</small>
+				</Toast.Header>
+
+				<Toast.Body>
+					You need to be logged in to access this functionality.
+				</Toast.Body>
+			</Toast>
+		</nav>
+	);
 }
 
 export default Navigation;
